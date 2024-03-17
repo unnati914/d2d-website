7702d1954b4595f03940aa5d650948cdefd7f351
Rajat Rajput <hello.rajat.rajput@gmail.com>
2024-03-17 19:20:45 +0530

    pushed react template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MainPage from "./components/MainPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header /> 
+      <MainPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
